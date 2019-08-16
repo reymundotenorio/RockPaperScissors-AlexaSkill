1f72f8def4650b34011de902403242398e1f5567
Reymundo Tenorio <reymundotenorio@gmail.com>
2019-08-16 16:05:05 -0700

    Adding LaunchIntent, askHowManyWins state, and views

--- src/app/states/main.states.js
+++ src/app/states/main.states.js
@@ -1,7 +1,25 @@
 function register(voxaApp) {
-  voxaApp.onIntent("LaunchIntent", {
-    flow: "terminate",
-    reply: "Launch.StartResponse",
+  // voxaApp.onIntent("LaunchIntent", {
+  //   flow: "terminate",
+  //   reply: "Launch.StartResponse",
+  // });
+
+  // This will run when the user launches the skill 
+  voxaApp.onIntent("LaunchIntent", () => {
+    return {
+      flow: "continue",
+      reply: "Welcome",
+      to: "askHowManyWins",
+    };
+  });
+
+  // This one will ask the amounts of wins necessary to beat, or be beaten
+  voxaApp.onState("askHowManyWins", () => {
+    return {
+      flow: "yield",
+      reply: "AskHowManyWins",
+      to: "getHowManyWins",
+    };
   });
 }
 
